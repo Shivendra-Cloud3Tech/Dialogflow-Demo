04f9ae7200fc19576943c6ca8f4add28c489fcbc
Shivendra-Cloud3Tech <146200282+Shivendra-Cloud3Tech@users.noreply.github.com>
2023-11-23 19:29:37 +0530

    Add files via upload

---
+++ ChatbotReq/src/main/java/com/bot/model/Message.java
@@ -0,0 +1,14 @@
+package com.bot.model;
+
+public class Message {
+
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
